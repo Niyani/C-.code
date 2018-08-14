dcddf225032f7f61895364a7cc2bd490806d0fa7
Niyani <41764533+Niyani@users.noreply.github.com>
2018-08-14 22:01:45 +0800

    Create IftreePostOrder.c
    输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果

---
+++ interview/IftreePostOrder.c
@@ -0,0 +1,32 @@
+bool verifysquenceofbst(vector<int> sequence)
+	{
+			if (sequence.empty())
+				return false;
+			int len = sequence.size();
+			vector<int> len_left;
+			vector<int> len_right;
+			//找到左子树，左子树肯定比根的值小
+			int i = 0;
+			for (; i < len - 1; i++){
+				if (sequence[i] < sequence[len - 1])
+					len_left.push_back(sequence[i]);
+				else
+					break;
+			}
+			//右子树--右子树的值肯定比根的值大
+			for (; i < len - 1; i++){
+				if (sequence[i] > sequence[len - 1]){
+					len_right.push_back(sequence[i]);
+				}
+				else
+					return false;
+			}
+			//递归
+			bool left = true, right = true;
+			if (!len_left.empty())
+				left = verifysquenceofbst(len_left);
+			if (!len_right.empty())
+				right = verifysquenceofbst(len_right);
+			return left && right;
+
+		}	
