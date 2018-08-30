d7ce7dc31ec81f563b1da5c6f824e54cd868a651
Niyani <41764533+Niyani@users.noreply.github.com>
2018-08-30 23:14:56 +0800

    Update ReverseList.c

--- nowcode/ReverseList.c
+++ nowcode/ReverseList.c
@@ -17,3 +17,25 @@ public:
         return p1;
     }
 };
+//////////////////////////////////////////////////////
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+       if(pHead==NULL)
+            return NULL;
+        ListNode* pRe=NULL;
+        ListNode* pNext=NULL;
+        while(pHead!=NULL)
+        {
+            //先用next保存head的下一个节点的信息，保证单链表不会因为失去head节点的原next节点而就此断裂
+            pNext=pHead->next;
+            //保存完next，就可以让head从指向next变成指向pre了
+            pHead->next=pRe;
+             //head指向pre后，就继续依次反转下一个节点
+            //让pre，head，next依次向后移动一个节点，继续下一次的指针反转
+            pRe=pHead;
+            pHead=pNext;
+        }
+        return pRe;
+    }
+};
