4904dea74b7a1e047a53464457400a6313388e86
Niyani <41764533+Niyani@users.noreply.github.com>
2018-08-31 11:50:19 +0800

    Update PrintMinNumber.c

--- interview/PrintMinNumber.c
+++ interview/PrintMinNumber.c
@@ -1,3 +1,11 @@
+/*遇到这个题，全排列当然可以做，但是时间复杂度为O(n!)。在这里我们自己定义一个规则，对拼接后的字符串进行比较。
+排序规则如下：
+若ab > ba 则 a 大于 b，
+若ab < ba 则 a 小于 b，
+若ab = ba 则 a 等于 b；
+根据上述规则，我们需要先将数字转换成字符串再进行比较，因为需要串起来进行比较。比较完之后，按顺序输出即可。
+*/
+
 class Solution {
 public:
     string PrintMinNumber(vector<int> numbers) {
